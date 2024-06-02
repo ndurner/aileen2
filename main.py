751c84c776be5990f2868ec394110cb44fa25fe2
Nils Durner <n.durner@namirial.com>
2024-06-02 23:44:42 +0200

    infrastructure

---
+++ main.py
@@ -0,0 +1,13 @@
+import argparse
+from dotenv import load_dotenv
+
+def handle_command_line_args():
+    parser = argparse.ArgumentParser(description="AI Task Agent")
+    parser.add_argument('--task', type=str, help='Task to be executed')
+    args = parser.parse_args()
+    if args.task:
+        print(f"Received task via command line: {args.task}")
+
+if __name__ == "__main__":
+    handle_command_line_args()
+    load_dotenv()
